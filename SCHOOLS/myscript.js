4bfcf6bb2dacb5ffe20f2eef38594eff318bea59
Om Prakash Pandey <om0000092@gmail.com>
2022-01-17 20:44:05 +0530

    Initial Commit

---
+++ SCHOOLS/myscript.js
@@ -0,0 +1,13 @@
+function mymath(){
+location.href="math.html"
+}
+function myenglish(){
+    location.href="english.html"
+    }
+    function mycomputer(){
+        location.href="computer.html"
+        }
+        function myscience()
+        {
+            location.href="science.html"
+            }
